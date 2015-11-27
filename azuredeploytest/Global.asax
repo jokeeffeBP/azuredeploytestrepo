74f4a712cd0f4f67ce054efe76404a4f8cc5af6c
James O'Keeffe <jaokeeffe@outlook.com>
2015-11-28 00:09:07 +1100

    Initial commit

---
+++ azuredeploytest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="azuredeploytest.MvcApplication" Language="C#" %>
---
+++ azuredeploytest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(azuredeploytest.Startup))]
+namespace azuredeploytest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
