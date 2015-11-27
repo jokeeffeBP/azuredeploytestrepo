7689fa73ef11c10b04387ff43b8873c7afb575ac
James O'Keeffe <jaokeeffe@outlook.com>
2015-11-28 00:23:56 +1100

    Fixed

--- azuredeploytest/Controllers/HomeController.cs
+++ azuredeploytest/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace azuredeploytest.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your failing application description page.";
+            ViewBag.Message = "Your application description page.";
 
             return View();
         }
